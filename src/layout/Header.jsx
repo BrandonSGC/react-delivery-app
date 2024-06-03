252bd5d14aa8b99e3001e8d36c9a7e815649b170
BrandonSGC <brangmz20@gmail.com>
2024-06-02 20:45:18 -0600

    Header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header contenedor">
+      <div className="header__logo">
+        <img src="/img/logo.svg" alt="Delivery App logo" />
+      </div>
+
+      <nav className="navegacion">
+        <Link to="/" className="navegacion__link">
+          Iniciar Sesion
+        </Link>
+        <Link to="/" className="navegacion__link">
+          Crear Cuenta
+        </Link>
+        <Link to="/" className="navegacion__link navegacion__link--registrar">
+          Registrar Restaurante
+        </Link>
+      </nav>
+    </header>
+  );
+};
